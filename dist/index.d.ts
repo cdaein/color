485d10f77eee85f963041a8d0377e52a3424a83c
user <you@example.com>
2022-10-22 20:50:33 -0400

    0.1.0 dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,7 @@
 /**
- * default data type is array for any color.
- *
- * */
-/**
  * convert normalized HSB color to RGB
  * @param arr [h, s, v] or [h, s, v, a]
+ * @returns RGB color array[3 or 4] in 0..255
  */
 export declare function hsv2rgb(arr: number[]): number[];
 /**
@@ -12,6 +9,7 @@ export declare function hsv2rgb(arr: number[]): number[];
  * @param h normalized hue 0..1 (wrapped)
  * @param s normalized saturation 0..1 (clamped)
  * @param v normalized brightness 0..1 (clamped)
+ * @returns RGB color array[3] in 0..255
  */
 export declare function hsv2rgb(h: number, s: number, v: number): number[];
 /**
@@ -20,7 +18,12 @@ export declare function hsv2rgb(h: number, s: number, v: number): number[];
  * @param s normalized saturation 0..1 (clamped)
  * @param v normalized brightness 0..1 (clamped)
  * @param a alpha 0..1 passed-through
+ * @returns RGB color array[4] in 0..255
  */
 export declare function hsv2rgb(h: number, s: number, v: number, a: number): number[];
 export declare const hsb2rgb: typeof hsv2rgb;
+export declare function rgb2hsv(arr: number[]): number[];
+export declare function rgb2hsv(r: number, g: number, b: number): number[];
+export declare function rgb2hsv(r: number, g: number, b: number, a: number): number[];
+export declare function hex2rgb(): void;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,6 @@
 "use strict";
-/**
- * default data type is array for any color.
- *
- * */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.hsb2rgb = exports.hsv2rgb = void 0;
+exports.hex2rgb = exports.rgb2hsv = exports.hsb2rgb = exports.hsv2rgb = void 0;
 const hsvrgb = require("hsv2rgb");
 /**
  * convert normalized HSB color to RGB
@@ -12,6 +8,7 @@ const hsvrgb = require("hsv2rgb");
  * @param s normalized saturation 0..1 (clamped)
  * @param v normalized brightness 0..1 (clamped)
  * @param a alpha 0..1 passed-through
+ * @returns RGB color array in 0..255
  */
 function hsv2rgb(hOrArr, s, v, a) {
     if (s !== undefined && v !== undefined && a !== undefined) {
@@ -37,4 +34,12 @@ function hsv2rgb(hOrArr, s, v, a) {
 }
 exports.hsv2rgb = hsv2rgb;
 exports.hsb2rgb = hsv2rgb;
+function rgb2hsv(rOrArr, g, b, a) {
+    return [];
+}
+exports.rgb2hsv = rgb2hsv;
+function hex2rgb() {
+    //
+}
+exports.hex2rgb = hex2rgb;
 //# sourceMappingURL=index.js.map
